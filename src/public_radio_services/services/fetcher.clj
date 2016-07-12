4eb0d559bbd74677d6d3d43530cf83e1ba5c6c1a
ags2121 <alexsilva88@gmail.com>
2016-07-12 14:08:22 -0400

    add more pods

--- src/public_radio_services/services/fetcher.clj
+++ src/public_radio_services/services/fetcher.clj
@@ -3,7 +3,8 @@
             [clojure.core.async :refer [chan go >! <!]]
             [clojure.data.json :as json]
             [environ.core :refer [env]]
-            [clojure.data.xml :as xml]))
+            [clojure.data.xml :as xml]
+            [clojure.data.xml.name :as name]))
 
 (def NPR-API-KEY (env :npr-api-key))
 (def NPR-ENDPOINT (str "https://api.npr.org/query?id=500005&profileTypeId=15"
@@ -22,70 +23,74 @@
 (defn- ^:private get-xml-node-attribute [parent-node tag attr]
   (-> (get-xml-node parent-node tag) :attrs attr))
 
-(defn- ^:private rss-parser [text rss-url]
+(defn- ^:private xml-parser [text rss-url showTitle]
   (let [parsed-xml (xml-seq (xml/parse-str text))
-        showTitle (->> parsed-xml
-                            (filter #(= (:tag %) :title))
-                            first
-                            :content
-                            first)
+        showTitle (or showTitle (->> parsed-xml
+                                     (filter #(= (:tag %) :title))
+                                     first :content first))
+        genre (some->> parsed-xml
+                       (filter #(= (:tag %) (name/canonical-name "http://www.itunes.com/dtds/podcast-1.0.dtd" "category" "itunes")))
+                       first :attrs first val)
         showUrl (->> parsed-xml
-                          (filter #(= (:tag %) :link))
-                          first
-                          :content
-                          first)
+                     (filter #(= (:tag %) :link))
+                     first :content first)
         first-item (->> parsed-xml
                         (filter #(= (:tag %) :item))
                         first)
         episodeTitle (get-xml-node-content first-item :title)
         pubDate (get-xml-node-content first-item :pubDate)
         url (get-xml-node-attribute first-item :enclosure :url)]
-    {:url url :pubDate pubDate :episodeTitle episodeTitle :rssUrl rss-url :showTitle showTitle :showUrl showUrl}))
+    {:url url :pubDate pubDate :episodeTitle episodeTitle :rssUrl rss-url :showTitle showTitle :showUrl showUrl :genre genre}))
 
-(defn- ^:private npr-parser [response _]
+;; currently only implemented for npr specific endpoints
+(defn- ^:private api-parser [response _ _]
   (let [json-response (json/read-str response)
         latest-story (get-in json-response ["list" "story" 0])
         story-url (get-in latest-story ["audio" 0 "format" "mp3" 0 "$text"])
         story-date (get-in latest-story ["storyDate" "$text"])]
     {:url story-url :pubDate story-date}))
 
-(defrecord Resource [type url parser])
+(defrecord Resource [type url parser showTitle])
 
-(defn resource
-  ([type url parser] (->Resource type url parser))
-  ([type url] (->Resource type url rss-parser)))
+(defn xml-resource
+  ([type url] (->Resource type url xml-parser nil))
+  ([type url showTitle] (->Resource type url xml-parser showTitle)))
+
+(defn api-resource [type url]
+  (->Resource type url api-parser nil))
 
 (def ^:private NEWSCAST-ENDPOINTS
-  [(resource :npr NPR-ENDPOINT npr-parser)
-   (resource :pri "http://www.pri.org/programs/3704/episodes/feed")
-   (resource :bbc-global "http://www.bbc.co.uk/programmes/p02nq0gn/episodes/downloads.rss")
-   (resource :marketplace "http://feeds.publicradio.org/public_feeds/marketplace-pm/rss/rss")
-   (resource :democracynow "http://www.democracynow.org/podcast.xml")
-   (resource :pbs "http://feeds.feedburner.com/NewshourFullProgramPodcast?format=xml")
-   (resource :bbc-africa "http://www.bbc.co.uk/programmes/p02nrtyw/episodes/downloads.rss")])
+  [(api-resource :npr NPR-ENDPOINT)
+   (xml-resource :pri "http://www.pri.org/programs/3704/episodes/feed")
+   (xml-resource :bbc-global "http://www.bbc.co.uk/programmes/p02nq0gn/episodes/downloads.rss")
+   (xml-resource :marketplace "http://feeds.publicradio.org/public_feeds/marketplace-pm/rss/rss")
+   (xml-resource :democracynow "http://www.democracynow.org/podcast.xml")
+   (xml-resource :pbs "http://feeds.feedburner.com/NewshourFullProgramPodcast?format=xml")
+   (xml-resource :bbc-africa "http://www.bbc.co.uk/programmes/p02nrtyw/episodes/downloads.rss")])
 
 (def ^:private PODCAST-ENDPOINTS
-  [(resource :reveal "http://feeds.revealradio.org/revealpodcast.xml")
-   (resource :nypl "http://newyorkpubliclibrary.libsyn.com/rss")
-   (resource :in-our-time "http://www.bbc.co.uk/programmes/b006qykl/episodes/downloads.rss")
-   (resource :open-source "http://radioopensource.org/feed/")
-   (resource :radiolab "http://feeds.wnyc.org/radiolab")
-   (resource :radiotonic "http://www.abc.net.au/radionational/feed/5421356/podcast.xml")
-   (resource :factmag "http://factmag.squarespace.com/factmixes?format=RSS")
-   (resource :homebrave "http://feeds.feedburner.com/homebravepodcast")
-   (resource :rumble "http://www.rumblestripvermont.com/feed/")
-   (resource :ideas "http://www.cbc.ca/podcasting/includes/ideas.xml")
-   (resource :unfictional "http://feeds.kcrw.com/kcrw/uf")
-   (resource :organist "http://feeds.kcrw.com/kcrw/to")
-   (resource :shortcuts "http://www.bbc.co.uk/programmes/b01mk3f8/episodes/downloads.rss")
-   (resource :seriously "http://www.bbc.co.uk/programmes/p02pc9qx/episodes/downloads.rss")])
+  [(xml-resource :reveal "http://feeds.revealradio.org/revealpodcast.xml")
+   (xml-resource :nypl "http://newyorkpubliclibrary.libsyn.com/rss")
+   (xml-resource :in-our-time "http://www.bbc.co.uk/programmes/b006qykl/episodes/downloads.rss")
+   (xml-resource :open-source "http://radioopensource.org/feed/" "Open Source")
+   (xml-resource :radiolab "http://feeds.wnyc.org/radiolab")
+   (xml-resource :radiotonic "http://www.abc.net.au/radionational/feed/5421356/podcast.xml" "Radiotonic")
+   (xml-resource :factmag "http://factmag.squarespace.com/factmixes?format=RSS" "FACT Mixes")
+   (xml-resource :homebrave "http://feeds.feedburner.com/homebravepodcast")
+   (xml-resource :rumble "http://www.rumblestripvermont.com/feed/")
+   (xml-resource :ideas "http://www.cbc.ca/podcasting/includes/ideas.xml" "CBC Radio's Ideas")
+   (xml-resource :unfictional "http://feeds.kcrw.com/kcrw/uf")
+   (xml-resource :organist "http://feeds.kcrw.com/kcrw/to")
+   (xml-resource :shortcuts "http://www.bbc.co.uk/programmes/b01mk3f8/episodes/downloads.rss")
+   (xml-resource :seriously "http://www.bbc.co.uk/programmes/p02pc9qx/episodes/downloads.rss")
+   (xml-resource :bodegaboys "http://feeds.soundcloud.com/users/soundcloud:users:169774121/sounds.rss")
+   (xml-resource :snapjudgement "http://feeds.wnyc.org/snapjudgment-wnyc")
+   (xml-resource :worldinwords "http://feeds.feedburner.com/pri/world-words")
+   ])
 
-(defn ^:private get-ajax-channel [news-source]
-  (let [c (chan)
-        url (:url news-source)
-        type (:type news-source)
-        parser (:parser news-source)]
-    (httpkit/get url #(go (>! c {type (parser (:body %) url)})))
+(defn ^:private get-ajax-channel [{:keys [url type parser showTitle]}]
+  (let [c (chan)]
+    (httpkit/get url #(go (>! c {type (parser (:body %) url showTitle)})))
     c))
 
 (defn ^:private get-resources [endpoints]
