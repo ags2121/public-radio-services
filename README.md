173d89767efab78124de7ba0f294b06ded6b29a3
Alex Silva <alex.silva@adp.com>
2022-12-23 13:01:31 -0500

    add cors headers

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # public radio services
 
-```python
+```
 python3 -m venv env
 source env/bin/activate
 pip install -i https://pypi.python.org/simple -r requirements.txt
 
 NUM_WORKERS=8 python fetch_urls.py
+
+curl "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+
+# open chrome browser
+open -na Google\ Chrome --args --user-data-dir=/tmp/temporary-chrome-profile-dir --disable-web-security
+
 ```
--- app.py
+++ app.py
@@ -5,6 +5,12 @@ def handler(event, context):
     feeds = get_feeds_wrapper.perform_web_requests()
     return {
         "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
+        "headers": {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+            "Access-Control-Allow-Credentials": "true",
+            "Content-Type": "application/json"
+        },
         "body": feeds,
     }
